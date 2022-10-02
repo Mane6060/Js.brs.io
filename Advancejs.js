218c6b08f1300df4300d8b24c82759e0fa38e13b
Mane6060 <107363707+Mane6060@users.noreply.github.com>
2022-10-02 10:57:30 -0700

    first commit

---
+++ Advancejs.js
@@ -0,0 +1,18 @@
+//Prototypes
+
+
+
+function Animal(name, color, age){
+
+    this.aname = name;
+    this.acolor = color;
+    this.aage = age;
+}
+
+
+
+const anml = new Animal("dog","black","4");
+
+
+console.log(anml.acolor);
+
---
+++ es.js
@@ -0,0 +1,23 @@
+//console.log(a);
+
+//var a = 10;
+//console.log (a);
+
+
+
+
+//let name = "AJINKY"
+//console.log(name);
+
+var name = "felix"
+
+name = "hello"
+
+console.log(name);
+
+//let const ===== Block  scope
+
+//var ==== function scope
+
+
+
